e485ef8fce49673c4f8207ff5442c57f8a0007d3
elkhour <elkhour23@gmail.com>
2026-09-24 11:15:17 +0300

    database

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecomerce',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
